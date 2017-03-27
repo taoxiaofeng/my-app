b8fa994e090e09e2cca9cb1b7ed046e1f719480d
taoxf <taoxf@ipharmacare.net>
2017-03-27 22:29:13 +0800

    Individual data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'My First Angular App!';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'windstorm'
+  }
 }
-   
