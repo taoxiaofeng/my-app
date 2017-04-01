79899dc1c7e9a9472f364ca4f062a957eecd804f
taoxf <taoxf@ipharmacare.net>
2017-04-01 21:43:50 +0800

    主从结构

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,19 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +22,7 @@ import { Hero } from './hero';
 
 export class AppComponent {
   title = 'Tour of Heroes';
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: 'windstorm'
